6dc2a1ea80e5b9ccfb01a3050f1df3853ccf627c
Talal Al-Tamimi <ttamimi@users.noreply.github.com>
2017-02-15 17:41:25 +0000

    PT1-202 consumer monitor thread never dies (#2)

--- lib/consumer_monitor.rb
+++ lib/consumer_monitor.rb
@@ -18,11 +18,14 @@ module HiveHome
 
       def start
         Thread.new do
-          begin
-            run
-          rescue => e
-            puts "Error in consumer data monitor: #{e}"
-            puts "#{e.backtrace}"
+          while true
+            begin
+              run
+            rescue => e
+              puts "Error in consumer data monitor: #{e}"
+              puts e.backtrace
+            end
+            sleep 60
           end
         end
       end
