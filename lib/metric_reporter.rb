a134dd9b42d5f0b7744eca466e44d6314aece1b9
Talal Tamimi <talal.al-tamimi@hivehome.com>
2018-09-11 15:48:17 +0100

    PT1-2707 temporary fix for kafka-topic-monitor until 3rd-party library is patched

--- lib/metric_reporter.rb
+++ lib/metric_reporter.rb
@@ -77,10 +77,26 @@ module HiveHome
       private
 
       def report_kafka_metrics
-        time             = Time.new
+        time = Time.new
         consumer_offsets = @consumer_monitor.get_consumer_offsets
-        all_topics       = @data_retriever.topics - ['__consumer_offsets']
-        topic_offsets    = @data_retriever.last_offsets_for(*all_topics)
+
+        # TODO: workaround for PT1-2707
+        # Remove the following rescue and retry logic after 3rd party library fix is released
+        # See https://github.com/zendesk/ruby-kafka/issues/642
+        attempts = 0
+        begin
+          attempts += 1
+          all_topics = @data_retriever.topics - ['__consumer_offsets']
+        rescue ::Kafka::ConnectionError => e
+          if attempts == 1
+            @data_retriever.instance_variable_get(:@cluster).mark_as_stale!
+            retry
+          else
+            raise e
+          end
+        end
+
+        topic_offsets = @data_retriever.last_offsets_for(*all_topics)
 
         report_end_offsets(time, topic_offsets)                      if @opts.report_end_offsets
         report_consumer_offsets(time, consumer_offsets)              if @opts.report_consumer_offsets
